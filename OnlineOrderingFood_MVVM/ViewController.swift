59151facfd52ce2dd83ecdfe02fc55ffbf319a49
Xursandbek <xursandbek06082001@mail.com>
2023-07-11 10:47:13 +0500

    Initial Commit

---
+++ OnlineOrderingFood_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineOrderingFood_MVVM
+//
+//  Created by Xursandbek Qambaraliyev on 11/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
